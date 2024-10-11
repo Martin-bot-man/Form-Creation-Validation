dae61630d43c9ef08f9cc486ae775d9941ca415c
Martin Odongo <martinwere14@gmail.com>
2024-10-11 16:02:05 +0300

    executed the logic part of the form

--- script.js
+++ script.js
@@ -5,5 +5,26 @@ const feedbackDiv = document.getElementById('form-feedback');
 form.addEventListener("submit", function(event){
       event.preventDefault();
 
+      const username = document.getElementById('username') .value.trim();
+      const email = document.getElementById('email').value.trim();
+      const password = document.getElementById('password').value.trim();
+
+    let isValid = true;
+    let messages = [];
+   if(username.length<3){
+isValid=false;
+   
+messages.push("Username must be atleast 3 characters long")
+return;
+} 
+if(!email.includes(@)||!email.includes('.')){
+    isValid=false
+    messages.push("Not a valid email");
+
+}
+if(password.length<8){
+    isValid =false;
+    messages.push('Password length is too short')
+}
 })
 })
