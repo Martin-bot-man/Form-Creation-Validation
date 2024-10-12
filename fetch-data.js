08efe4f7319a5f4212cc16007d37dc9bb68c226c
Martin Odongo <martinwere14@gmail.com>
2024-10-13 01:48:53 +0300

    fetched username from the api

--- fetch-data.js
+++ fetch-data.js
@@ -3,14 +3,23 @@ async function fetchUserData(){
     const dataContainer = document.getElementById('api-data')
     try {
       const response =await fetch(apiUrl)
+      console.log(response);
       const users =await response.json(response)
       dataContainer.innerHTML='';
       const userList = document.createElement("ul");
-      userList.forEach(user =>{
-        const listItem = document.createElement('li');
-        listItem.textContent =`${username} `
-        dataContainer.append(userList);
+
+      users.forEach(function(res) {
+
+        const userName = res.name 
+        console.log(res.name);
+        const listItem =document.createElement('li');
+        listItem.innerText= userName;
+        userList.append(listItem)
+        console.log(userName)
+
       })
+
+
     } catch (error) {
         dataContainer.innerHTML='Failed to load user data.';
     }
