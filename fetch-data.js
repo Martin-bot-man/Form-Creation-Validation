9ca8efec6bb7334a3a078cfd2e513b8badc87d93
Martin Odongo <martinwere14@gmail.com>
2024-10-12 05:33:38 +0300

    completed the data fetching while learning DOM

--- fetch-data.js
+++ fetch-data.js
@@ -1,4 +1,20 @@
 async function fetchUserData(){
     const apiUrl = 'https://jsonplaceholder.typicode.com/users'
-    
+    const dataContainer = document.getElementById('api-data')
+    try {
+      const response =await fetch(apiUrl)
+      const users =await response.json(response)
+      dataContainer.innerHTML='';
+      const userList = document.createElement("ul");
+      userList.forEach(user =>{
+        const listItem = document.createElement('li');
+        listItem.textContent =`${username} `
+        dataContainer.append(userList);
+      })
+    } catch (error) {
+        dataContainer.innerHTML='Failed to load user data.';
+    }
 }
+document.addEventListener('DOMContentLoaded',()=>{
+    fetchUserData();
+})
